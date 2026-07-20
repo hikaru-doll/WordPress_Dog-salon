2c28cdb985590184842ca3b808b3e81b50b60bf9
hikaru <makinaminami@gmail.com>
2026-07-20 23:26:58 +0900

    リストではなく<article>に変更

--- page-staff.php
+++ page-staff.php
@@ -10,16 +10,14 @@ get_header();
 ?>
 
 <!-- main start ---------------------------------------------->
-<main id="page-staff" class="main">
+<main class="main page-staff">
   <div class="wrapper">
     <?php
     $theme_uri = esc_url(get_theme_file_uri()); ?>
     <?php if (have_posts()): while (have_posts()): the_post(); ?>
         <h1 class="page-title"><?php the_title(); ?></h1>
-
-
-        <ul class="list grid">
-          <li class="item">
+        <div class="list grid">
+          <article class="item">
             <div class="image">
               <img src="<?php echo $theme_uri; ?>/assets/imgs/staff1.webp" alt="ジョン" />
             </div>
@@ -28,7 +26,7 @@ get_header();
               Johnは当サロンのトリミングのプロフェッショナルで、JKC（日本犬協会）公認のトリマー資格を持っています。<br />
               彼は犬の美容に情熱を傾け、一匹一匹の犬の個性を尊重しながら、最高のトリミングを提供します。犬との絆を深め、飼い主の皆様にもトリミングが楽しいひとときとなるよう心がけています。
             </p>
-          </li>
+          </article>
           <li class="item">
             <div class="image">
               <img src="<?php echo $theme_uri; ?>/assets/imgs/staff2.webp" alt="リサ" />
@@ -38,7 +36,7 @@ get_header();
               Lisaは当サロンの誇るJDA（日本ドッグアート協会）公認トリマーです。<br>
               彼女は犬の美容に対する情熱と技術力を兼ね備えており、犬の美しさを最大限に引き立てるために努力しています。お客様の要望に応じ、犬の個性に合わせたトリミングを提供し、笑顔でお出迎えいたします。 </p>
           </li>
-          <li class="item">
+          <article class="item">
             <div class="image">
               <img src="<?php echo $theme_uri; ?>/assets/imgs/staff3.webp" alt="エマ" />
             </div>
@@ -46,8 +44,8 @@ get_header();
             <p class="text">
               Emmaは美容師から転身したトリマーとして、犬の美容に新しい視点をもたらしています。<br>
               彼女は美容の世界で培ったスキルを活かし、犬たちに特別なケアを提供します。犬たちにとってリラックスできる環境を大切にし、飼い主の皆様にも安心感を提供します。</p>
-          </li>
-          <li class="item">
+          </article>
+          <article class="item">
             <div class="image">
               <img src="<?php echo $theme_uri; ?>/assets/imgs/staff4.webp" alt="マイケル" />
             </div>
@@ -55,11 +53,10 @@ get_header();
             <p class="text">
               Michaelは当サロンのアシスタントとして、スタッフ全体をサポートしています。<br>
               彼は犬たちへの愛情深い接し方で知られ、トリミングプロセスがより円滑に進行するお手伝いをします。笑顔とポジティブなエネルギーで、お客様とその愛犬たちに楽しいひとときを提供します。 </p>
-          </li>
+          </article>
         <?php endwhile; ?>
-      <?php else: ?>
       <?php endif; ?>
-        </ul>
+        </div>
   </div>
 </main>
 <!-- /main end  ---------------------------------------------------->
