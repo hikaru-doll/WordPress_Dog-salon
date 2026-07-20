5d432dc146ebc1b8838b7503320716567aeb4591
hikaru <makinaminami@gmail.com>
2026-07-20 15:20:41 +0900

    画像のパス間違いを修正

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@ defined('ABSPATH') || exit;
     <p class="text">
       お探しのページはありませんでした。URLが正しくないかもしれません。
     </p>
-    <div class="dog-icon"><img src="<?php echo esc_url(get_theme_file_uri()); ?>assets/image/dog.svg" alt="" /></div>
+    <div class="dog-icon"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/assets/imgs/dog.svg" alt="柴犬のイラスト" /></div>
     <div class="button"><a href="<?php home_url(); ?>">TOPページへ戻る</a></div>
   </div>
 </main>
